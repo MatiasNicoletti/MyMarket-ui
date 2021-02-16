c0cd24dc83445680b18cd21de7891c8e5a1e5bf7
Matias Nicoletti <matiasnicolettimdp@gmail.com>
2021-02-16 00:17:14 -0300

    header(nav+logo) style base done

--- mymarket-ui/src/app/presentation/presentation.module.ts
+++ mymarket-ui/src/app/presentation/presentation.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 import { HomeComponent } from './views/home/home.component';
+import { LogoComponent } from './ui/logo/logo.component';
+import { HeaderComponent } from './ui/header/header.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    LogoComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
