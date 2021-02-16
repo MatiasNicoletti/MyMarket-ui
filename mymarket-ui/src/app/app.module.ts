cdec95ba6d7e5885bddfd4db87321e7dd27b3ef9
Matias Nicoletti <matiasnicolettimdp@gmail.com>
2021-02-15 22:48:43 -0300

    app routing added

--- mymarket-ui/src/app/app.module.ts
+++ mymarket-ui/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UiModule } from './presentation/ui/ui.module';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    UiModule
+    CommonModule,
+    UiModule,
+    AppRoutingModule
   ],
   providers: [],
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
