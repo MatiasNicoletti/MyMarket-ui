b4adb1ebba3b4fce012c34da33d091365ac68e57
Matias Nicoletti <matiasnicolettimdp@gmail.com>
2021-02-28 22:51:34 -0300

    https changed

--- mymarket-ui/src/app/app.component.ts
+++ mymarket-ui/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AwsAuthService } from './core/auth/aws-auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mymarket-ui';
+
+  constructor(
+    private aws: AwsAuthService
+  ){}
+  ngOnInit(): void {
+    this.aws.listen();
+  }
+
+  
+
+  
 }
--- mymarket-ui/src/app/core/auth/aws-auth.service.ts
+++ mymarket-ui/src/app/core/auth/aws-auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Auth } from 'aws-amplify';
-
+import { Auth, Hub } from 'aws-amplify';
+import { CognitoUser, ISignUpResult } from 'amazon-cognito-identity-js'
 @Injectable({
   providedIn: 'root'
 })
@@ -15,18 +15,40 @@ export class AwsAuthService {
         region: 'us-east-1',
         domain: 'mymarket.auth.us-east-1.amazoncognito.com',
         scope: ['email', 'openid', 'aws.cognito.signin.user.admin'],
-        redirectSignIn: 'https://mymarket.vercel.app',
-        redirectSignOut: 'https://mymarket.vercel.app',
+        redirectSignIn: 'https://127.0.0.1:4200',
+        redirectSignOut: 'https://127.0.0.1:4200',
         responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
-      } 
+      }
     })
-   }
+  }
 
-  hostedUISignin(){
+  hostedUISignin() {
     Auth.federatedSignIn().then(result => {
       console.log(result);
     }).catch(err => {
       console.log(err);
+    });
+
+
+  }
+
+  listen() {
+    console.log('llego al listen')
+    Hub.listen("auth", async ({ payload: { event, data } }) => {
+      //tanto en eventos de signIn y signOut cargamos al current user y hacemos update a la UI
+      console.log(event, data)
+      switch (event) {
+        case "signIn":
+          console.log('signin');
+          var currentUser = <CognitoUser>(await Auth.currentAuthenticatedUser());
+          console.log('getCurrentUser', currentUser);
+          break;
+        case "signOut":
+          console.log('signOut')
+          break;
+        case "customOAuthState":
+          alert('custom state')
+      }
     });
   }
 }
