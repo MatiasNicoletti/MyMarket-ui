d76ed72ae668f93de8a5c0a0bfbbdb8596bb5559
Matias Nicoletti <matiasnicolettimdp@gmail.com>
2021-03-07 17:03:32 -0300

    offer main view progress

--- mymarket-ui/src/app/presentation/views/offer/offer.component.ts
+++ mymarket-ui/src/app/presentation/views/offer/offer.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-offer',
-  templateUrl: './offer.component.html',
-  styleUrls: ['./offer.component.css']
+  templateUrl: './offer.component.html'
 })
 export class OfferComponent implements OnInit {
 
